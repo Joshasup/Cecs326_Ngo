ff1acf4da0b21a395514d4dd4bf75f3669ce67b3
Joshasup <hasupsong@gmail.com>
2019-02-26 18:26:23 -0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,6 +1,39 @@
-//
-// Created by christopher on 2/15/19.
-//
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <random>
 
-int main() {
+using namespace std;
+
+int main(){
+
+    int acknowledge = 0;
+    struct buf {
+        long mtype;
+        char message[50];
+
+    }
+
+    buf msg;
+    int size = sizeof(msg) - sizeof(long);
+    msg.mtype = 997;
+
+    while (true){
+
+        int randomNum = rand();
+        if (randomNum < 100){
+            //delete
+        }
+
+        else if ((acknowledge = 1) && ((randomNum%msg.mtype)== 0))
+        {
+            //send
+            
+        }
+    }
 }
