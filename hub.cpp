f3bea9538bb1e64c7d8047a320cac9045e72e71c
BornIncompetence <cagbrothers@gmail.com>
2019-03-21 14:16:22 -0700

    Removed unnessesary string copies. Improved safety

--- hub.cpp
+++ hub.cpp
@@ -6,11 +6,12 @@
 #include "force_patch.h"
 #include <cstring>
 #include <iostream>
+#include <optional>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
 struct queue {
-    // Create message queue
+    // Create message queue when program begins
     queue() {
         qid = msgget(ftok(".", 'u'), IPC_EXCL | IPC_CREAT | 00666);
         if (qid == -1) {
@@ -21,7 +22,7 @@ struct queue {
         std::cout << "Using QID of " << qid << std::endl;
     }
 
-    // Delete message queue
+    // Delete message queue when program ends
     ~queue() {
         int status = msgctl(qid, IPC_RMID, nullptr);
         std::cout << "Program finished with code " << status << "\n";
@@ -41,102 +42,38 @@ pid_t getpid() {
     return i;
 }
 
-// Code used to test route A
-void a_route(int qid) {
-    message_buffer msg{shared_mtype};
-    long probe_a_mtype = 2;
+// Split message into two, print the message, and set pid of Probes if they
+// haven't been yet
+std::optional<pid_t> parse_message(message_buffer &m,
+                                   std::optional<pid_t> &a_pid,
+                                   std::optional<pid_t> &b_pid,
+                                   std::optional<pid_t> &c_pid) {
 
-    ssize_t status = msgrcv(qid, &msg, msg_size, shared_mtype, IPC_NOWAIT);
-    while (std::string_view{msg.message} != std::string_view{"TERM"}) {
-        // TODO: Fix this condition
-
-        // The message was able to be read from Probe A
-        if (status != -1) {
-            // Send acknowledgement back to ProbeA
-            // TODO: Decode that the number is indeed from ProbeA using modulo
-            std::cout << "Probe [A]: " << std::string_view{msg.message} << "\n";
-            msg.message_type = probe_a_mtype;
-            strncpy(msg.message, "Received from Probe A", sizeof(msg.message));
-            msgsnd(qid, &msg, msg_size, 0);
-        }
-        // Attempt to read the next message from Probe A
-        msg.message_type = shared_mtype;
-        status = msgrcv(qid, &msg, msg_size, shared_mtype, IPC_NOWAIT);
+    // The pivot is set by the first strtol to the address before the next token
+    // So getting the next int is a matter of reading one token ahead
+    char *pivot;
+    auto id = std::strtol(m.message, &pivot, 10);
+    auto num = std::strtol(pivot + 1, nullptr, 10);
+    printf("[%li]: %li", id, num);
+    if (valid_reading(num, 274471)) {
+        a_pid = id;
+        return a_pid;
+    } else if (valid_reading(num, 80107)) {
+        b_pid = id;
+        return b_pid;
+    } else if (valid_reading(num, 75479)) {
+        c_pid = id;
+        return c_pid;
     }
-    std::cout << "ProbeA has been terminated.\n";
+    return std::nullopt;
 }
 
-// Code used to test route B
-void b_route(int qid, pid_t pid) {
-    message_buffer msg{shared_mtype};
-    std::cout << "Waiting for Probe B...\n";
-    int message_count = 0;
-    while (message_count < 10'000) {
-        ssize_t status =
-            msgrcv(qid, &msg, msg_size, msg.message_type, IPC_NOWAIT);
-
-        // The message was able to be read
-        if (status != -1) {
-            std::cout << "Probe [B]: " << std::string_view{msg.message} << "\n";
-            ++message_count;
-        }
-    }
-    std::cout << "Limit reached, killing ProbeB.\n";
-    force_patch(pid);
-}
-
-// Code used to test route C
-void c_route(int qid) {
-    message_buffer c{3};
-    message_buffer msg{shared_mtype};
-    std::cout << "Waiting for Probe B...\n";
-
-    auto kill_status = msgrcv(qid, &c, msg_size, c.message_type, IPC_NOWAIT);
-    while (kill_status == -1) {
-        ssize_t status =
-            msgrcv(qid, &msg, msg_size, msg.message_type, IPC_NOWAIT);
-
-        // The message was able to be read
-        if (status != -1) {
-            std::cout << "Probe [C]: " << std::string_view{msg.message} << "\n";
-        }
-        kill_status = msgrcv(qid, &c, msg_size, c.message_type, IPC_NOWAIT);
-        if (kill_status != -1) {
-            break;
-        }
-    }
-    std::cout << "Final message from ProbeC: " << std::string_view{c.message}
-              << std::endl;
-}
-
-enum pids { A, B, C, D };
-
-// Split string into two
-auto parse_message(message_buffer &m, int &a_pid, int &b_pid, int &c_pid) {
-    std::string a{m.message};
-    auto pivot = a.find_first_of(':');
-    std::string pid(a.begin(), a.begin() + pivot);
-    std::string num(a.begin() + pivot + 1, a.end());
-
-    auto i = std::atoi(num.c_str());
-    if (valid_reading(i, 274471)) {
-        a_pid = std::atoi(pid.c_str());
-        return pids::A;
-    } else if (valid_reading(i, 80107)) {
-        b_pid = std::atoi(pid.c_str());
-        return pids::B;
-    } else if (valid_reading(i, 75479)) {
-        c_pid = std::atoi(pid.c_str());
-        return pids::C;
-    }
-}
-
+// Returns true if it detects ProbeA's kill message
 bool a_end(int qid, message_buffer &m) {
     ssize_t kill_status = msgrcv(qid, &m, msg_size, m.message_type, IPC_NOWAIT);
-    // Kill message received when A sends final message
-    // Message received
+    // Kill message received when ProbeA produces number less than 100
     if (kill_status != -1) {
-        // Message is valid
+        // Message is valid, condition may not be needed...
         if (std::string_view{m.message} == "TERM") {
             std::cout << "Last ProbeA message: " << std::string_view{m.message}
                       << std::endl;
@@ -147,6 +84,7 @@ bool a_end(int qid, message_buffer &m) {
     return false;
 }
 
+// Returns true if it detects ProbeC's kill_patch message
 bool c_end(int qid, message_buffer &m) {
     ssize_t kill_status = msgrcv(qid, &m, msg_size, m.message_type, IPC_NOWAIT);
     // Kill message received when C is killed by separate process
@@ -167,28 +105,28 @@ void general_route(queue &q) {
     message_buffer a_rcv{4};
     strncpy(a_snd.message, "Acknowledged.", sizeof(a_snd.message));
     bool a_dead = false;
+    std::optional<pid_t> a_pid;
     // B setup
     int message_count = 0;
     bool b_dead = false;
+    std::optional<pid_t> b_pid;
     // C setup
     message_buffer c_msg{3};
     bool c_dead = false;
+    std::optional<pid_t> c_pid;
+
+    // Optionals are used as a form of type-safe lazy initialization
 
     while (!(a_dead && b_dead && c_dead)) {
         // Receive a message
         ssize_t status = msgrcv(qid, &shared_msg, msg_size,
                                 shared_msg.message_type, IPC_NOWAIT);
-        pid_t a_pid;
-        pid_t b_pid;
-        pid_t c_pid;
 
         // Message received
         if (status != -1 && std::string_view{shared_msg.message} != "") {
-            printf("Message: %s\n", shared_msg.message);
-
-            auto from = parse_message(shared_msg, a_pid, b_pid, c_pid);
-            // Send acknowledgement back
-            if (from == pids::A) {
+            // Send acknowledgement back if the message came from ProbeA
+            if (auto from = parse_message(shared_msg, a_pid, b_pid, c_pid);
+                a_pid && from == a_pid) {
                 msgsnd(qid, &a_snd, msg_size, 0);
             }
             ++message_count;
@@ -199,10 +137,17 @@ void general_route(queue &q) {
             a_dead = true;
         }
         // B termination
-        if (!b_dead && message_count == 10000) {
+        if (!b_dead && message_count >= 10000) {
             printf("Limit reached, killing ProbeB.\n");
-            force_patch(b_pid);
-            b_dead = true;
+
+            // If ProbeB hasn't been set, prevent program from killing a random
+            // process.
+            if (b_pid) {
+                force_patch(b_pid.value());
+                b_dead = true;
+            } else {
+                printf("Attempting to kill ProbeB, but ProbeB doesn't exist\n");
+            }
         }
         // C termination
         if (!c_dead && c_end(qid, c_msg)) {
@@ -217,8 +162,6 @@ int main() {
     // Create message queue shared by all 3 hubs
     queue q{};
 
-    // a_route(q.qid);
-    // b_route(q.qid, pid);
-    // c_route(q.qid);
+    // Begin main logic loop
     general_route(q);
 }
